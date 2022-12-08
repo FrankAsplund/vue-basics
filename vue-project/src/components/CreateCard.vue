72a7026c25e365d4a7c4514e958087a750eb0342
FrankAsplund <frank_asplund@hotmail.com>
2022-12-08 16:01:09 +0100

    Initial commit

---
+++ vue-project/src/components/CreateCard.vue
@@ -0,0 +1,73 @@
+<script>
+
+import axios from "axios";
+
+export default {
+    name: "CreateCard",
+    data() {
+    return {
+      formData :{
+        title: "",
+        text: ""
+      }
+      }
+    },
+
+    methods: {
+        createCard() {
+
+        axios.post("http://localhost:3000/posts", this.formData)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error))
+        }
+    }
+
+  } 
+</script>
+
+<template>
+
+<div class="input-container">
+    <form @submit.prevent="createCard">
+
+        <div class="card-h1">Create a new post</div>
+        <br>
+        <input type="text" placeholder="Title" v-model="formData.title">
+        <br>
+        <textarea cols="15" rows="5" placeholder="Text" v-model="formData.text">
+        </textarea>
+        <br>
+        <button> Create new card </button>
+    </form>
+    </div>
+
+</template>
+
+<style scoped>
+
+input {
+  height: 20px;
+  width: auto;
+}
+
+textarea {
+    margin-top: 10px;
+}
+
+.input-container {
+    padding: 10px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  
+  
+    border-style: none;
+    background-color: rgba(112, 128, 144, 0.14);
+    border-radius: 10px;
+    width: 300px;
+    height: auto;
+  }
+
+
+</style>
